f6a89c7300441e87f0b3b04b7d19ca9980ecb4bc
apon <apon.shariar@m2amedia.tv>
2020-10-19 12:27:34 +0600

    initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+from selenium import webdriver
+
+base_url = "http://drr.land.gov.bd/khatian/"
+
+suffix_from = int(input())
+suffix_to = int(input())
+search_string = input()
+# suffix_from = 15654000
+# suffix_to = 15654100
+# search_string = "নীলফামারী টাউন"
+
+driver = webdriver.Firefox()
+f = open("khotian_output.txt", "a")
+
+for suff in range(suffix_from, suffix_to, 1):
+    try_url = base_url + str(suff)
+    driver.get(try_url)
+    khotian_body = driver.find_element_by_xpath("/html/body")
+    print(khotian_body.text)
+
+    if search_string in khotian_body.text:
+        f.write("\n\nUrl : {} \n\n".format(try_url))
+        f.write("Khotian Data :\n\n {} \n".format(khotian_body.text))
+        f.write("\n--------------------------------------------------------------------------------------------\n\n\n")
+
+f.close()
+driver.close()
